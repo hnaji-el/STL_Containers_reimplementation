60851fc4249c13392520f555fae8e27827bb9ca2
hnaji-el <hamidnaji981@gmail.com>
2022-05-17 10:24:07 +0100

    .....

--- vector/main.cpp
+++ vector/main.cpp
@@ -1,8 +1,55 @@
 
-#include "vector.hpp"
+//#include "vector.hpp"
+#include <iostream>
+
+struct Bar
+{
+	Bar(void)						{ std::cout << "default Ctor [Bar]   " << std::endl; }
+	Bar(int)						{ std::cout << "int Ctor [Bar]       " << std::endl; }
+	Bar(Bar const &) 				{ std::cout << "copy Ctor [Bar]      " << std::endl; }
+	void	operator=(Bar const &)	{ std::cout << "copy assignment [Bar]" << std::endl; }
+	~Bar(void)						{ std::cout << "Dtor [Bar]           " << std::endl; }
+};
+
+struct Foo
+{
+	Bar		_obj;
+
+	Foo(void) : _obj()
+	{
+		std::cout << "default Ctor [Foo]" << std::endl;
+	}
+	Foo(int) : _obj(100)
+	{
+		std::cout << "int Ctor [Foo]" << std::endl;
+	}
+	Foo(Bar const & obj = Bar()) : _obj(obj)
+	{
+		std::cout << "Bar Ctor [Foo]" << std::endl;
+	}
+	Foo(Foo const &)
+	{
+		std::cout << "copy Ctor [Foo]" << std::endl;
+	}
+	~Foo(void)
+	{
+		std::cout << "Dtor [Foo]" << std::endl;
+	}
+};
 
 int	main(void)
 {
+	std::cout << "#################################" << std::endl;
+	Bar		b;
+	std::cout << "#################################" << std::endl;
+	Foo		a(b);
+	std::cout << "#################################" << std::endl;
+
 	return (0);
 }
 
+/*
+ * SYNTAX: 
+ * _obj(Bar())   === _obj()
+ * _obj(Bar(10)) === _obj(10)
+ */
--- vector/vector.tpp
+++ vector/vector.tpp
@@ -5,6 +5,12 @@
 namespace ft
 {
 
+template<class T, class Alloc>
+explicit vector<T, Alloc>::vector(const allocator_type& alloc = allocator_type())
+	: _array(NULL), _alloc(alloc), _size(0), _capacity(0)
+{
+}
+
 }
 
 #endif
